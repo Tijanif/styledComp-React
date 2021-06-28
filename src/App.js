2d36d3a2b867466f4625c41d3fd536840927374a
Tijani Fulani <Fulani07@gmail.com>
2021-06-28 19:36:10 +0100

    moved buttons to elements. js

--- src/App.js
+++ src/App.js
@@ -20,37 +20,6 @@ const Subhead = styled.h3`
   font-weight: 600;
   margin: 160px 0 48px;
 `;
-const Buttons = styled.a`
-  height: 48px;
-  line-height: 48px;
-  padding: 0 64px;
-  font-weight: 600;
-  border-radius: 100px;
-  color: ${colors.black};
-  background-color: ${colors.white};
-  cursor: pointer;
-  margin: 0 24px;
-  font-size: 14px;
-  text-decoration: none;
-
-  ${(props) =>
-    props.outline &&
-    css`
-      background: transparent;
-      border: 1px solid ${colors.black};
-    `}
-
-  ${(props) =>
-    props.primary &&
-    css`
-      background: ${colors.primary};
-    `}
-      ${(props) =>
-    props.secondary &&
-    css`
-      background: ${colors.secondary};
-    `}
-`;
 
 function App() {
   return (
